6fe73aca50b57ea8f0462cff9c2e5be76fca3828
CindyBarria <cindybarria@gmail.com>
2020-07-27 18:44:14 -0400

    Primeros avances

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('fs');
+
+fs.readdir('./', (error, files) => {
+  if (error) {
+    console.log(error)
+  }
+  console.log(files)
+});
+
+
+fs.readFile('PRUEBA.md', 'UTF-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(data)
+});
+
