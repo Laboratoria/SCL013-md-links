fe07aec35895e7f604f2078146e4f8dd6a1db079
Stefany <efyguevara@gmail.com>
2020-08-05 18:34:40 -0400

    pruebas test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+/*const mdLinks = require('../');
 
 
 describe('mdLinks', () => {
@@ -7,4 +7,36 @@ describe('mdLinks', () => {
     console.log('FIX ME!');
   });
 
+});
+
+const axios = require('axios');
+
+const promises = {
+  fetchUser:() => axios
+  .get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.data)
+  .catch(error => 'Ejecutando Error')
+}
+
+module.exports = promises;
+*/
+/*
+const promises = require('../src/readMd')
+test ('User fetch shold be Leann Graham', ()=>{
+  expect.assertions(1);
+  const data = await promises.fecthUser();
+  expect(data.name).toEqual('Leanne Graham');
+  expect(data)
+})
+*/
+
+
+
+const readMD = require('../src/readMd');
+
+test('readMD', () => {
+  expect.assertions(1)
+  return readMD.then(readMD => {
+    expect(readMD(path)).toBe('function');
+  });
 });
